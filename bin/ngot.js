2b9476794af641fa080535d3a3f4c037ec5bd001
ngot <zhuanghengfei@gmail.com>
2023-01-16 18:40:14 +1100

    feat: make it work on windows

---
+++ bin/ngot.js
@@ -0,0 +1,6 @@
+const { execSync } = require("child_process");
+
+execSync("git config user.name 'ngot'");
+execSync("git config user.email 'zhuanghengfei@gmail.com'");
+
+console.log("set user.name & user.email ok");
